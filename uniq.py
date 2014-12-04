76a20480ae1c2abaf7c6cb1fe8bbe2aba7ca3388
Gautier Hayoun <ghayoun@gmail.com>
2014-12-04 20:49:57 +0000

    Add Comparaison functions

--- uniq.py
+++ uniq.py
@@ -17,7 +17,40 @@ parser.add_argument('-i',)
 
 uniques = Counter()
 
+class Comparaison(object):
+    def __init__(self, *args):
+        pass
+
+    def cmp(self, value1, value2):
+        return value1 == value2
+    
+
+class IntComparaison(Comparaison):
+    def cmp(self, value1, value2):
+        return int(value1) == int(value2)
+
+
+class DecimalPlacesComparaison(Comparaison):
+    def __init__(self, decimal_places):
+        self.decimal_places = decimal_places
+
+    def cmp(self, value1, value2):
+        def parts(v):
+            int_part = int(v)
+            fractional_part = float(v) - int_part
+            relevant_fractional_part = round(fractional_part * 10 ** self.decimal_places)
+            return (int_part, relevant_fractional_part)
+
+        return parts(value1) == parts(value2)
+
 if __name__ == "__main__":
+    if "test" in sys.argv:
+        d = DecimalPlacesComparaison(2)
+        assert d.cmp(3.14, 3.144)
+        assert not d.cmp(3.14, 3.149)
+        assert not d.cmp(2, 3)
+
+    
     if sys.argv[1:]:
         for file_name in sys.argv[1:]:
             with open(file_name) as file_handle:
