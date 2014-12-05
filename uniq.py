8cd9c48cda72dd938ffb8e131ce1d864e11867cd
Gautier Hayoun <ghayoun@gmail.com>
2014-12-05 09:44:11 +0000

    Add fuzzy matching proof of concept

--- uniq.py
+++ uniq.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from collections import Counter
 import argparse
@@ -9,6 +9,8 @@ import sys
 parser = argparse.ArgumentParser(description='Worker out uniqueness')
 parser.add_argument('-d',  dest='delimiter', metavar='D', type=str,
                     default=',', help='case insensitive output')
+parser.add_argument('-C',  dest='comparator', type=str, default='Comparaison',
+                    choices=('i', 'dp'))
 
 uniques = Counter()
 
@@ -25,13 +27,14 @@ class IntComparaison(Comparaison):
         return int(value1) == int(value2)
 
 
+import math
 class DecimalPlacesComparaison(Comparaison):
     def __init__(self, decimal_places):
         self.decimal_places = decimal_places
 
     def cmp(self, value1, value2):
         def parts(v):
-            int_part = int(v)
+            int_part = math.floor(float(v))
             fractional_part = float(v) - int_part
             relevant_fractional_part = round(fractional_part * 10 ** self.decimal_places)
             return (int_part, relevant_fractional_part)
@@ -46,8 +49,31 @@ class LineGenerator(object):
 
     def __iter__(self):
         for line in self.filehandle:
-            yield tuple(line.split(self.options.delimiter))
+            #yield line.split(self.options.delimiter)
+            yield line
 
+
+def comparable(value):
+    comparator = DecimalPlacesComparaison(2)
+
+    class Wrapper(object):
+        def __init__(self, value):
+            self.value = value
+
+        def __eq__(self, other):
+            return comparator.cmp(self.value, other.value)
+
+        def __repr__(self):
+            return self.value
+
+        def __hash__(self):
+            return 1
+
+    return Wrapper(value)
+
+def decimal_places_reduce(options, file_handle=None):
+    for line in LineGenerator(options, file_handle):
+        yield comparable(line.strip('\n'))
 
 if __name__ == "__main__":
     if "test" in sys.argv:
@@ -60,9 +86,9 @@ if __name__ == "__main__":
     if sys.argv[1:]:
         for file_name in sys.argv[1:]:
             with open(file_name) as file_handle:
-                uniques.update(Counter(LineGenerator(options, file_handle)))
+                uniques.update(Counter(decimal_places_reduce(options, file_handle)))
     else:
-        uniques.update(Counter(LineGenerator(options)))
-    print uniques.most_common()
+        uniques.update(Counter(decimal_places_reduce(options)))
+    print (uniques.most_common())
 
 
